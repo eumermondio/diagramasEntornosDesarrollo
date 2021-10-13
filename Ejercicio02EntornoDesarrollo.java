25c74101b7ef4ff739e3c1ebba01c8f14a0d8f16
eumer <73362204+eumermondio@users.noreply.github.com>
2021-10-13 10:36:35 +0200

    Add files via upload

---
+++ Ejercicio02EntornoDesarrollo.java
@@ -0,0 +1,22 @@
+package capitulo03.bloque02;
+
+import java.util.Scanner;
+
+public class Ej02EntDes {
+
+	public static void main(String[] args) {
+
+		int num, suma = 0;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("Dame un número");
+			num = sc.nextInt();
+			suma += num;
+		}while (num != 0); 
+
+		System.out.println("La suma es: " + suma);
+		
+		sc.close();
+	}
+
+}
---
+++ Ejercicio03.java
@@ -0,0 +1,22 @@
+package capitulo03.bloque02;
+
+import java.util.Scanner;
+
+public class Ejercicio03EntDes {
+
+	public static void main(String[] args) {
+
+		int num, suma = 0, cont = 0, cont2 = 1;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("Dime el número " + cont2++);
+			num = sc.nextInt();
+			suma += num;
+			cont++;
+		} while (cont < 10);
+
+		System.out.println("La suma es: " + suma);
+		sc.close();
+	}
+
+}
