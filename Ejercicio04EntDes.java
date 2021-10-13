df58411505b639f6a57696b8771ad508e41c5359
eumer <73362204+eumermondio@users.noreply.github.com>
2021-10-13 18:11:57 +0200

    Add files via upload

---
+++ Ejercicio04EntDes.java
@@ -0,0 +1,24 @@
+package capitulo03.bloque02;
+
+import java.util.Scanner;
+
+public class Ejercicio04EntDes {
+
+	public static void main(String[] args) {
+
+		int num = 0, num1 = 0;
+		Scanner sc = new Scanner(System.in);
+		do {
+
+			System.out.println("Dime el primer número");
+			num = sc.nextInt();
+			System.out.println("Dime el segundo número");
+			num1 = sc.nextInt();
+			
+
+		} while (num != num1);
+		sc.close();
+
+	}
+
+}
