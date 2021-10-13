5b62834ca40d69c4eba086c0422b5b8609870260
eumer <73362204+eumermondio@users.noreply.github.com>
2021-10-13 18:29:37 +0200

    Add files via upload

---
+++ Ejercicio05EntDes.java
@@ -0,0 +1,36 @@
+package capitulo03.bloque02;
+
+import java.util.Scanner;
+
+public class Ejercicio05EntDes {
+
+	public static void main(String[] args) {
+
+		int num1, suma = 0, repes, cont2 = 1;
+		float cont = 0;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Cuantos números quieres operar");
+		repes = sc.nextInt();
+
+		for (int i = 0; i < repes; i++) {
+
+			System.out.println("Dime el número " + cont2++);
+			num1 = sc.nextInt();
+
+			if (num1 > 0) {
+				cont++;
+				suma += num1;
+			}
+			if (num1 < 0) {
+				System.out.println("Mete un número positivo");
+				break;
+			}
+
+		}
+
+		System.out.println("La media de los números es: " + (suma / cont));
+		sc.close();
+	}
+
+}
